abda9b4d0b2fa9aee103575114620127ae1878ac
Stas <staske11111@gmail.com>
2022-05-18 20:55:43 +0300

    NoteDto is changed(added text). Authorize is off

--- Notes.Application/Notes/Queries/NoteDto.cs
+++ Notes.Application/Notes/Queries/NoteDto.cs
@@ -9,6 +9,7 @@ namespace Notes.Application.Notes.Queries
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Text { get; set; }
 
         public void Mapping(Profile profile)
         {
